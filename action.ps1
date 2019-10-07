8b0dd1008d25b8e1343554af16c3f54448ab8aa6
Eugene Bekker <ebekker@users.noreply.github.com>
2019-10-07 12:26:27 -0400

    adding persistence, buildnum bumping and exporting values

--- action.ps1
+++ action.ps1
@@ -42,11 +42,17 @@ $stateGistBanner = @"
 */
 "@
 
+## We represent each scope as its own class to form a tree structure of nodes
+## and this also gives us the ability to add more metadata fields in the future
+class VersionBuildNum {
+    [int]$build_num = 0
+}
+
 class WorkflowBuildNum {
     [int]$build_num = 0
     [System.Collections.Generic.Dictionary[
         string, int]]$version_buildnums =
-            [System.Collections.Generic.Dictionary[string, int]]::new()
+            [System.Collections.Generic.Dictionary[string, VersionBuildNum]]::new()
 }
 
 class GlobalBuildNum {
@@ -102,14 +108,85 @@ class GlobalBuildNum {
     }
 
     ## Create initial state data for specified version key if it doesn't exist
-    if ($version_key -and (
-        -not $stateData.workflow_buildnums[$workflow_name].ContainsKey($version_key))) {
+    if ($version_key -and (-not $stateData.workflow_buildnums[
+            $workflow_name].version_buildnums.ContainsKey($version_key))) {
         Write-ActionDebug "BuildNum state for Version not found, initializing"
-        $stateData.workflow_buildnums[$workflow_name][$version_key] = 0
+        $stateData.workflow_buildnums[$workflow_name].version_buildnums[$version_key] =
+            [VersionBuildNum]::new()
     }
 
-    Write-ActionDebug "Resolved current state data:"
+    if (-not $stateGist) {
+        Write-ActionInfo "Creating initial state Gist"
+        $createGistResp = Invoke-WebRequest -Headers $apiHeaders -Uri $gistsApiUrl -Method Post -Body (@{
+            public = $false
+            files = @{
+                $stateGistName = @{
+                    content = @"
+$stateGistBanner
+$($stateData | ConvertTo-Json -Depth 10)
+"@
+                }
+            }
+        } | ConvertTo-Json)
+        $createGist = $createGistResp.Content | ConvertFrom-Json -AsHashtable
+        $stateGist = $createGist
+    }
+
+    Write-ActionDebug "Resolved starting state data:"
     Write-ActionDebug ($stateData | ConvertTo-Json -Depth 10)
+
+    if (-not $skip_bump) {
+        Write-ActionInfo "Bumping up version numbers"
+
+        $stateData.build_num += 1;
+        Write-Debug "New Global build_num = [$($stateData.build_num)]"
+
+        $stateData.workflow_buildnums[$workflow_name].build_num += 1;
+        Write-Debug "New Workflow build_num = [$($stateData.workflow_buildnums[$workflow_name].build_num)]"
+
+        if ($version_key) {
+            $stateData.workflow_buildnums[$workflow_name].version_buildnums[
+                $version_key].build_num += 1
+            Write-Debug "New Workflow build_num = [$($stateData.workflow_buildnums[$workflow_name].version_buildnums[
+                $version_key].build_num)]"
+        }
+
+        Write-ActionDebug "Resolved updated state data:"
+        Write-ActionDebug ($stateData | ConvertTo-Json -Depth 10)
+
+        Write-ActionInfo "Updating state Gist"
+        $patchGistUrl = "$gistsApiUrl/$($metadataGist.id)"
+        $patchGistResp = Invoke-WebRequest -Headers $apiHeaders -Uri $patchGistUrl -ErrorAction Stop -Method Patch -Body (@{
+            files = @{
+                $metadataFilename = @{
+                    content = @"
+$stateGistBanner
+$($stateData | ConvertTo-Json -Depth 10)
+"@
+                }
+            }
+        } | ConvertTo-Json)
+    }
+
+    Write-ActionDebug "Setting outputs"
+    Set-ActionOutput global_build_number ($stateData.build_num)
+    Set-ActionOutput workflow_build_number ($stateData.workflow_buildnums[
+        $workflow_name].build_num)
+    if ($version_key) {
+        Set-ActionOutput version_build_number ($stateData.workflow_buildnums[
+            $workflow_name].version_buildnums[$version_key].build_num)
+    }
+
+    if ($set_env) {
+        Write-ActionDebug "Setting env vars"
+        Set-ActionVariable BUILDNUM_FOR_GLOBAL ($stateData.build_num)
+        Set-ActionVariable BUILDNUM_FOR_WORKFLOW ($stateData.workflow_buildnums[
+            $workflow_name].build_num)
+        if ($version_key) {
+            Set-ActionVariable BUILDNUM_FOR_VERSION ($stateData.workflow_buildnums[
+                $workflow_name].version_buildnums[$version_key].build_num)
+        }
+    }
 
 # # # }
 # # # catch {
